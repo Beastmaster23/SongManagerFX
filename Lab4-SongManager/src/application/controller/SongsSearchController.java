4c7713b56a90206b868198d094d462f7e55c59d6
Interco <edn1211@gmail.com>
2019-06-19 23:48:19 -0500

    init

---
+++ Lab4-SongManager/src/application/controller/SongsSearchController.java
@@ -0,0 +1,24 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class SongsSearchController {
+	@FXML
+    private TextField songTextFeild;
+
+    @FXML
+    private TextArea consoleText;
+
+    @FXML
+    void searchTextFeildListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchTextButton(ActionEvent event) {
+
+    }
+}
